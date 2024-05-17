c09afb5fbdf57f5db4ee391df377b319504476a5
AssPimple <123962900+AssPimple@users.noreply.github.com>
2024-05-17 18:35:40 +0300

    update

--- pars.py
+++ pars.py
@@ -1,30 +1,84 @@
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from fake_useragent import UserAgent
+import time
+
+a = []
 
 
-def getlinks(url):
+def add_domain(url, domain):
+    if 'facebook' in url or 'instagram' in url:
+        return domain
+
+    if not url.startswith('http'):
+        if url.startswith('/'):
+            return domain + url
+        else:
+            return domain + '/' + url
+    return url
+
+
+def get_dynamic_links(url):
     ua = UserAgent()
-    headers = {
-        'User-Agent': str(ua.random)
-    }
+    options = webdriver.ChromeOptions()
+    options.add_argument(f"user-agent={ua.random}")
+
+    # Указываем путь к драйверу браузера
+    service = Service(ChromeDriverManager().install())
+
+    # Создаем экземпляр драйвера браузера
+    driver = webdriver.Chrome(service=service, options=options)
+
+    # Загружаем страницу
+    driver.get(url)
+    time.sleep(2)
 
     # Получаем содержимое страницы
-    page = requests.get(url, headers=headers)
-    soup = BeautifulSoup(page.text, 'html.parser')
+    page_source = driver.page_source
 
-    # Находим все теги <div>
-    div_tags = soup.find_all('div')
+    driver.quit()
 
-    # Выводим текст из каждого найденного тега <div>
-    for div_tag in div_tags:
-        print(div_tag.text)
-        break
+    soup = BeautifulSoup(page_source, 'html.parser')
+    links = soup.find_all('a')
+    for link in links:
+        a.append(link.get('href'))
+
+
+def parse_website(url, domain):
+    k = 0
+    # Отправляем GET-запрос к веб-сайту
+    global links
+    url.pop(0)
+    url.pop(7)
+    with open('text.txt', 'w', encoding='utf-8') as file:
+        for i in url:
+            if i is not None:
+                k += 1
+                ua = UserAgent()
+                headers = {
+                    'User-Agent': str(ua.random)
+                }
+                # Получаем содержимое страницы
+                domain1 = add_domain(i, domain)
+                page = requests.get(domain1, headers=headers)
+                soup = BeautifulSoup(page.text, 'html.parser')
+
+                # Находим все теги <a>, которые содержат ссылки
+                links = soup.find_all('div')
+                for link in links:
+                    cleaned_text = link.text.strip()
+                    if cleaned_text:
+                        file.write(cleaned_text + ' ')
+                        break
 
 
 if __name__ == '__main__':
-    # Пример ссылки
-    url = 'https://аэронет2035.рф/catalog/tproduct/607938184-455698780031-geoskan-lite'
+    urli = 'https://аэронет2035.рф/'
 
-    # Парсим данные по ссылке
-    getlinks(url)
+    get_dubnynamic_links(urli)
+    print(a)
+    print(len(a))
+    parse_website(a, urli)
